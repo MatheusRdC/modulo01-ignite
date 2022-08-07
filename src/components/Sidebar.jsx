1e34ff6b26cf786608a1921840337a43d19d7a34
Matheus <carvalhoribeiro09@gmail.com>
2022-08-07 10:11:10 -0300

    Revert "style-sidebar"

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,3 @@
-import { PencilLine } from 'phosphor-react';
-
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -11,14 +9,12 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-               <img className={styles.avatar} src="https://github.com/matheusrdc.png" />
-
                 <strong>Matheus Carvalho</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#"> <PencilLine size={20} /> Editar seu perfil</a>
+                <a href="#">Editar seu perfil</a>
             </footer>
         </aside>
     );
