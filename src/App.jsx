554adb7e474e5e71dcea25f94820d9c31224852a
MatheusRdC <carvalhoribeiro09@gmail.com>
2022-08-09 19:53:29 -0300

    content-post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
+ 
 import './styles/global.css';
 
 export function App() {
@@ -13,14 +14,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Matheus Carvalho"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi modi fugit repellat optio voluptatem quisquam! Soluta reprehenderit voluptas aperiam quam totam sunt, nihil veritatis minus rerum cupiditate mollitia adipisci."
-          />
-          <Post
-            author="Lucas Eduardo"
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo necessitatibus laboriosam, eum, ratione vitae ullam error dignissimos vero consequuntur aliquid nulla in aliquam. Iure exercitationem ex eligendi facilis pariatur doloribus."
-          />
+         <Post />
         </main>
       </div>
     </>
